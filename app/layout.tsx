f2c05d78030a5a893acfd8376b3ad6476703954a
Fernando Masetto <masettofernando@gmail.com>
2025-05-08 12:41:58 -0300

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,10 +1,13 @@
+import "@/app/ui/global.css"
+
 export default function RootLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" >
+      <h1>NEXT</h1>
       <body>{children}</body>
     </html>
   );
